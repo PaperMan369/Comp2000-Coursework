9f30f91a3fecbb412ca48045f2b22453851b1765
Leo Lewis <leolewis1369@gmail.com>
2023-01-20 13:09:20 +0000

    Get request created and tested

--- app/src/main/java/com/example/comp2000_coursework/MainActivity.java
+++ app/src/main/java/com/example/comp2000_coursework/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.comp2000_coursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-    RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-    String url ="https://www.metaweather.com/api/location/search/?query=london";
+    RequestQueue queue;
+    String url;
+    public MainActivity(){
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-}
+    public void get(View view){
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        final TextView textView = new TextView(this);
 
+        queue = Volley.newRequestQueue(this);
+        url = "http://web.socem.plymouth.ac.uk/COMP2000/api/Employees";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("not working " + error.toString());
+                    }
+        });
+        linearLayout.addView(textView);
+        queue.add(stringRequest);
+
+
+    }
+}
